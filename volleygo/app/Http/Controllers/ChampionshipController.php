5dc0a85484fdd8df35f07d68f1891612f1b0f3c7
Caiqui Viera <cvcommunitymanager@gmail.com>
2022-07-15 18:15:13 -0300

    controller update

--- volleygo/app/Http/Controllers/ChampionshipController.php
+++ volleygo/app/Http/Controllers/ChampionshipController.php
@@ -9,22 +9,22 @@ class ChampionshipController extends Controller
     //Controlador de Campeonatos
 
     public function index () {
-        return 'Esta es la lista de campeonatos';
+        //mostrar todos los campeonatos
     }
 
     public function show($id) {
-        return 'Campeonato con id: '.$id;
+        //mostrar un campeonato
     }
 
     public function create() {
-        //
+        //crear un campeonato
     }
 
     public function update($id) {
-        //
+        //actualizar un campeonato
     }
 
     public function delete($id) {
-        //
+        //eliminar un campeonato
     }
 }
